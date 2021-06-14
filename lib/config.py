86a8299f0fdb1e357979fa57bcfc5b29db44ed63
TimHasert <timhasert@gmail.com>
2021-06-14 20:23:43 +0200

    config boilerplate

--- lib/config.py
+++ lib/config.py
@@ -1,5 +1,33 @@
 """
 lib/config.py
 
-config singleton
+bot config
 """
+
+import json
+
+
+class NightbotConfig:
+    """Nightbot config interface"""
+    
+    def __init__(self) -> None:
+        pass  # define nightbot fields
+
+
+class BotConfig:
+    """Bot config interface"""
+
+    def __init__(self) -> None:
+        pass
+
+    def from_file(self, path: str) -> None:
+        pass
+
+    def to_file(self, path: str) -> None:
+        pass
+
+    def from_dict(self, d: dict) -> None:
+        pass
+
+    def to_dict(self) -> dict:
+        pass
