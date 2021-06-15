da29a062013c4ac6d477df6632a74727e45d0c5e
TimHasert <timhasert@gmail.com>
2021-06-15 13:14:42 +0200

    remove scrape lib

--- __main__.py
+++ __main__.py
@@ -34,3 +34,28 @@ except KeyboardInterrupt:
 
 # create and run bot with config
 
+
+"""
+
+from pywinauto                      import Desktop
+from pywinauto.controls.uiawrapper  import UIAWrapper
+
+desk = Desktop(backend="uia")
+win = desk.windows()
+for i, w in enumerate(win):
+    print(i, w.window_text())
+
+i = int(input())
+b : UIAWrapper = win[i]
+
+
+
+def get_desktop(backend: str = 'uia') -> Desktop:
+    return Desktop(backend=backend)
+
+
+while True:
+    print(b.process_id())  # process id goes None when gone
+    print(b.window_text())
+    print(b.handle)
+"""
