f3b9f36794f4e962d1fce9c475ef1ff7b2dd5cd5
TimHasert <timhasert@gmail.com>
2021-06-15 13:11:02 +0200

    main boilerplate

--- __main__.py
+++ __main__.py
@@ -4,6 +4,26 @@ __main__.py
 script entrypoint
 """
 
+from argparse import ArgumentParser
+from bot import FoobarExtensionBot
+from config import BotConfig
+
+c = BotConfig(**{
+            'api' : {
+                'key'    : '',
+                'cmd_id' : 1
+            },
+            'interval' : 0.5,
+            'outtext'  : ''
+        })
+
+b = FoobarExtensionBot(c)
+
+try:
+    b.run()
+except KeyboardInterrupt:
+    b.stop()
+
 # parse config
 
 # build desktop object
@@ -13,3 +33,4 @@ script entrypoint
 # let user pick window
 
 # create and run bot with config
+
--- bot.py
+++ bot.py
@@ -3,18 +3,31 @@ bot.py
 """
 
 from config                         import BotConfig
+from threading                      import Event
 from pywinauto.controls.uiawrapper  import UIAWrapper
 
 
-class FoobarExtensionBot:
+class FoobarExtensionBot():
     """Bot polling a given window and posting its content to Nightbot API"""
 
     def __init__(self, config: BotConfig) -> None:
         self.config  : BotConfig  = config
+        self.stopped : Event      = Event()
         self.cur_out : str        = ''
 
     def run(self, window: UIAWrapper) -> None:
         """Run bot with a given window handle"""
         self.window : UIAWrapper = window
-        self._run()
+        self.stopped.clear()        
+        while not self.stopped.wait(2):
+            self._main_loop()
+        
+    def stop(self) -> None:
+        """Stops thread"""
+        self.stopped.set()
+
+    def _main_loop(self) -> None:
+        """Private run main loop"""
+        print('oiiii')
+        print(self.stopped.is_set(), self.stopped)
         
