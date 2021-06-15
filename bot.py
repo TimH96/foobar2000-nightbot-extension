fcf9720b0c47527256141714d39738bc3766628f
TimHasert <timhasert@gmail.com>
2021-06-15 12:07:18 +0200

    bot boilerplate

--- bot.py
+++ bot.py
@@ -1,10 +1,20 @@
+"""
+bot.py
+"""
 
+from config                         import BotConfig
+from pywinauto.controls.uiawrapper  import UIAWrapper
 
-from config import BotConfig
-
-BotConfig()
 
 class FoobarExtensionBot:
+    """Bot polling a given window and posting its content to Nightbot API"""
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, config: BotConfig) -> None:
+        self.config  : BotConfig  = config
+        self.cur_out : str        = ''
+
+    def run(self, window: UIAWrapper) -> None:
+        """Run bot with a given window handle"""
+        self.window : UIAWrapper = window
+        self._run()
+        
