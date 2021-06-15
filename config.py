5448745e28424cfe93194f52176a8e7fe0e45ef5
TimHasert <timhasert@gmail.com>
2021-06-15 09:23:57 +0200

    config implementation

--- config.py
+++ config.py
@@ -1,33 +1,33 @@
 """
-lib/config.py
+config.py
 
 bot config
 """
 
-import json
-
 
 class NightbotConfig:
     """Nightbot config interface"""
-    
-    def __init__(self) -> None:
-        pass  # define nightbot fields
+
+    def __init__(self, *, api_key: str, command_id: int) -> None:
+        self.key    : str = api_key
+        self.cmd_id : int = command_id
 
 
 class BotConfig:
     """Bot config interface"""
 
-    def __init__(self) -> None:
-        pass
-
-    def from_file(self, path: str) -> None:
-        pass
-
-    def to_file(self, path: str) -> None:
-        pass
-
-    def from_dict(self, d: dict) -> None:
-        pass
+    def __init__(self, *, interval: int, outtext: str, api: dict) -> None:
+        self.interval : int            = interval
+        self.outtext  : str            = outtext
+        self.api      : NightbotConfig = NightbotConfig(**api)
 
     def to_dict(self) -> dict:
-        pass
+        """Returns dict representation of """
+        return {
+            'api' : {
+                'key'    : self.api.key,
+                'cmd_id' : self.api.command_id
+            },
+            'interval' : self.interval,
+            'outtext'  : self.outtext
+        }
