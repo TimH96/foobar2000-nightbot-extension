93e310bb4190b8be05c8f8b6056a19c96b62e2d2
TimHasert <timhasert@gmail.com>
2021-06-16 16:18:26 +0200

    correctly prompt user to stop bot

--- __main__.py
+++ __main__.py
@@ -94,7 +94,7 @@ if __name__ == '__main__':
     # run bot
     print('----------------------')
     try:
-        print('Bot running ... CTRL+Z to stop')
+        print('Bot running ... CTRL+C to stop')
         bot.run(chosen_window)
     except KeyboardInterrupt:
         bot.stop()
